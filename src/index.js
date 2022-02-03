ec68de2d1d7cc90c50b9cbe1ca35bf90eaa71bad
SiegeTank-90 <ccline140@yahoo.com>
2022-02-03 16:33:40 -0600

    Update README.md, background-crew-desktop.jpg, and 80 more files...

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from "react-router-dom"
 import './index.css';
 import App from './App';
 
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,7 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
